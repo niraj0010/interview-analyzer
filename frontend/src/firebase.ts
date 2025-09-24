f60cb8a2a2c8499ee674e4614c7025a0835b4353
niraj0010 <niraj.bhatta@selu.edu>
2025-09-23 19:14:20 -0500

    Login and Signup with firebase auth

--- frontend/src/firebase.ts
+++ frontend/src/firebase.ts
@@ -1,4 +1,4 @@
-// src/firebase.ts
+
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
@@ -12,7 +12,6 @@ const firebaseConfig = {
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
-const app = initializeApp(firebaseConfig);
-
+const app = initializeApp(firebaseConfig)
 export const auth = getAuth(app);
 export const db = getFirestore(app);
